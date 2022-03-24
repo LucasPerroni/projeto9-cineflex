dc1815ec99a85b4cb292b143b774f31e69bacf27
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-03-23 21:34:55 -0300

    Base files

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Header from './Header/'
+import Movies from './Movies/'
+import Time from './Time/'
+import Seats from './Seats/'
+import Success from './Success/'
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Movies />} />
+                <Route path='/movie/:movieId' element={<Time />} />
+                <Route path='/section/:sectionId' element={<Seats />} />
+                <Route path='/success' element={<Success />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
---
+++ src/components/Time/index.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+export default function Time() {
+    return (
+        <></>
+    )
+} 

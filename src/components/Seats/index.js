1fcaa0b74405e7cf0c0ad630c4e0623a1aef4ac1
Lucas Bonde Perroni <lucasbondep2@gmail.com>
2022-03-25 14:01:15 -0300

    User name, cpf and seats are now stored in an obj

--- src/components/Seats/index.js
+++ src/components/Seats/index.js
@@ -8,7 +8,7 @@ import './style.css'
 export default function Seats() {
     const {sessionId} = useParams()
     const [session, setSession] = useState({})
-    const [selected, setSelected] = useState([])
+    const [user, setUser] = useState({name: '', cpf: '', ids: []})
 
     useEffect(() => {
         const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${sessionId}/seats`)
@@ -21,17 +21,22 @@ export default function Seats() {
             <main className='Seats'>
                 <h1>Select your seat(s)</h1>
                 <article>
-                    <DisplaySeats seats={session.seats} selected={selected} setSelected={setSelected}/>
+                    <DisplaySeats seats={session.seats} user={user} setUser={setUser}/>
                 </article>
                 <StatusType />
                 <section>
+
                     <p>Name:</p>
-                    <input placeholder='Write your name...'></input>
+                    <input placeholder='Write your name...' 
+                    onChange={e => setUser({...user, name: `${e.target.value}`})}></input>
+
                     <p>CPF:</p>
-                    <input type='number' placeholder='Write your CPF...'></input>
+                    <input type='number' placeholder='Write your CPF...'
+                    onChange={e => setUser({...user, cpf: `${e.target.value}`})}></input>
+                    
                 </section>
                 <div className='reserve'>
-                    <button>Reserve seat(s)</button>
+                    <button onClick={() => console.log(user)}>Reserve seat(s)</button>
                 </div>
             </main>
             <Footer id={session.movie.id} day={session.day.weekday} time={session.name} />
@@ -39,15 +44,16 @@ export default function Seats() {
     ) : (<p>Loading...</p>)
 } 
 
-function DisplaySeats({seats, selected, setSelected}) {
+function DisplaySeats({seats, user, setUser}) {
+    const {ids} = user
 
     function validation(id) {
-        if (!selected.includes(id)) {
-            return setSelected([...selected, id].sort( (a, b) => {return a - b} ))
+        if (!ids.includes(id)) {
+            return setUser({...user, ids: [...ids, id].sort( (a, b) => {return a - b} )})
         } else {
-            const index = selected.indexOf(id)
-            selected.splice(index, 1)
-            return setSelected([...selected].sort( (a, b) => {return a - b} ))
+            const index = ids.indexOf(id)
+            ids.splice(index, 1)
+            return setUser({...user, ids: [...ids].sort( (a, b) => {return a - b} )})
         }
     }
 
@@ -55,7 +61,7 @@ function DisplaySeats({seats, selected, setSelected}) {
         <>
             {seats.map( ({id, name, isAvailable}) => {
                 if (isAvailable) {
-                    return <button key={id} className={selected.includes(id) ? 'selected' : ''} 
+                    return <button key={id} className={ids.includes(id) ? 'selected' : ''} 
                     onClick={() => validation(id)}>{name}</button>
                 } else {
                     return <button key={id} className='unavailable' style={{'cursor': 'auto'}}>{name}</button>
